1db8ca1d3d3e6ac555816812b6f85d6c50a3c1c6
daido1976 <daido1976@gmail.com>
2023-03-30 20:15:43 +0900

    Set env for github actions

--- env.ts
+++ env.ts
@@ -2,4 +2,5 @@ import { load } from "https://deno.land/std@0.181.0/dotenv/mod.ts";
 
 const env = await load();
 
-export const lineChannelAccessToken = env["LINE_CHANNEL_ACCESS_TOKEN"];
+export const lineChannelAccessToken =
+  env["LINE_CHANNEL_ACCESS_TOKEN"] ?? Deno.env.get("LINE_CHANNEL_ACCESS_TOKEN");
