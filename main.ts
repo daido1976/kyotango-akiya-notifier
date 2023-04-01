b93c2bb5beaf3e1eeeaf28da44331458e2a7a885
daido1976 <daido1976@gmail.com>
2023-04-01 17:30:54 +0900

    Notify only when the count of akiya is increasing

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import { fetchAkiyaCount } from "./akiya-fetcher.ts";
+import { DB } from "./db.ts";
 import { notifyToBot } from "./notifier.ts";
+import { compareCount } from "./utils.ts";
 
 async function main() {
   // 1. 京丹後市の空き家バンクから空き家（賃貸のみ）の件数取得
@@ -8,11 +10,26 @@ async function main() {
     console.error("Failed to retrieve akiya count. Exiting the process.");
     exit(1);
   }
+  console.log(`The current count of akiya is ${akiyaCount}`);
 
-  // 2. 件数をgistに保存 & 前回の件数と比較して増えてるか判定
-  // TODO: 実装する
+  // 2. 前回の件数と比較して増えてるか判定 & 値が変わっていたら DB（Gist）に保存
+  const prevAkiyaCount = (await DB.get("chintaiAkiyaCount")) ?? 0;
+  const comparisonResult = compareCount(prevAkiyaCount, akiyaCount);
+  console.log(`The count of akiya is ${comparisonResult}`);
+
+  const shouldNotify = comparisonResult === "increased";
+  const akiyaCountHasChanged = comparisonResult !== "unchanged";
+
+  if (akiyaCountHasChanged) {
+    DB.set("chintaiAkiyaCount", akiyaCount);
+  }
 
   // 3. 増えてたら LINE ボットで通知
+  if (!shouldNotify) {
+    console.log("No need for notification as akiya count has not increased.");
+    exit();
+  }
+
   const ok = await notifyToBot(akiyaCount);
   if (!ok) {
     console.error("LINE bot notification failed.");
