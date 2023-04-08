f91eab3d277b23f16d705df82b6f6ddf6e0c5c7d
daido1976 <daido1976@gmail.com>
2023-04-08 11:00:28 +0900

    Impl getArrayChanges

--- utils.test.ts
+++ utils.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.181.0/testing/asserts.ts";
 import { describe, it } from "https://deno.land/std@0.181.0/testing/bdd.ts";
-import { compareCount, difference } from "./utils.ts";
+import { compareCount, difference, getArrayChanges } from "./utils.ts";
 
 describe("compareCount", () => {
   it("returns 'increased' when current count is greater than previous count", () => {
@@ -15,14 +15,48 @@ describe("compareCount", () => {
 });
 
 describe("difference", () => {
+  it("returns an empty array if the input arrays are empty", () => {
+    assertEquals(difference([], []), []);
+  });
+
   it("returns the correct difference of two arrays", () => {
     assertEquals(difference([1, 2, 3, 4], [2, 4, 6]), [1, 3]);
     assertEquals(difference([2, 4, 6], [1, 2, 3, 4]), [6]);
     assertEquals(difference(["a", "b", "c"], ["b"]), ["a", "c"]);
     assertEquals(difference(["foo", "bar"], []), ["foo", "bar"]);
   });
+});
 
-  it("returns an empty array if the input arrays are empty", () => {
-    assertEquals(difference([], []), []);
+describe("getArrayChanges", () => {
+  it("returns empty arrays when both oldArray and newArray are empty", () => {
+    assertEquals(getArrayChanges([], []), { added: [], removed: [] });
+  });
+
+  it("returns empty added and all elements of oldArray when newArray is empty", () => {
+    assertEquals(getArrayChanges([1, 2, 3], []), {
+      added: [],
+      removed: [1, 2, 3],
+    });
+  });
+
+  it("returns empty removed and all elements of newArray when oldArray is empty", () => {
+    assertEquals(getArrayChanges([], [1, 2, 3]), {
+      added: [1, 2, 3],
+      removed: [],
+    });
+  });
+
+  it("returns empty arrays when oldArray and newArray have the same elements", () => {
+    assertEquals(getArrayChanges([1, 2, 3], [1, 2, 3]), {
+      added: [],
+      removed: [],
+    });
+  });
+
+  it("returns added and removed elements when oldArray and newArray have different elements", () => {
+    assertEquals(getArrayChanges([1, 2, 3], [2, 3, 4]), {
+      added: [4],
+      removed: [1],
+    });
   });
 });
--- utils.ts
+++ utils.ts
@@ -14,3 +14,15 @@ export function compareCount(
 export function difference<T extends string | number>(a: T[], b: T[]): T[] {
   return a.filter((x) => !new Set(b).has(x));
 }
+
+export function getArrayChanges<T extends string | number>(
+  oldArray: T[],
+  newArray: T[]
+): {
+  added: T[];
+  removed: T[];
+} {
+  const added = difference(newArray, oldArray);
+  const removed = difference(oldArray, newArray);
+  return { added, removed };
+}
