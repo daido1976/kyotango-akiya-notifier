e53bdaa15f4d0e5383d1a97f8403be57e0c3170c
daido1976 <daido1976@gmail.com>
2024-01-05 16:56:08 +0900

    $ udd internal/deps.ts

--- internal/deps.ts
+++ internal/deps.ts
@@ -1,13 +1,13 @@
-export { load } from "https://deno.land/std@0.205.0/dotenv/mod.ts";
-export { parse } from "https://deno.land/std@0.205.0/flags/mod.ts";
+export { load } from "https://deno.land/std@0.211.0/dotenv/mod.ts";
+export { parse } from "https://deno.land/std@0.211.0/flags/mod.ts";
 
 // See. https://deno.land/manual@v1.36.1/advanced/jsx_dom/deno_dom
 export {
   DOMParser,
   Element,
-} from "https://deno.land/x/deno_dom@v0.1.42/deno-dom-wasm.ts";
+} from "https://deno.land/x/deno_dom@v0.1.43/deno-dom-wasm.ts";
 
 // for debug or test
-export { delay } from "https://deno.land/std@0.205.0/async/delay.ts";
-export { assertEquals } from "https://deno.land/std@0.205.0/assert/mod.ts";
-export { describe, it } from "https://deno.land/std@0.205.0/testing/bdd.ts";
+export { delay } from "https://deno.land/std@0.211.0/async/delay.ts";
+export { assertEquals } from "https://deno.land/std@0.211.0/assert/mod.ts";
+export { describe, it } from "https://deno.land/std@0.211.0/testing/bdd.ts";
